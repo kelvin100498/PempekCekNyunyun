63bf4c7b1c06f04cb14e47ab04f8196f5aff9335
kelvin <kelvin.aditya@weekendinc.com>
2024-06-20 10:46:52 +0700

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ import { Maps } from './components/Maps';
 const App = () => {
   return (
     <div className='h-full bg-pattern bg-repeat max-w-[1800px] mx-auto overflow-hidden'>
-      <a href="intent://link/ovo#Intent;scheme=https;package=com.pmi.limited.pmiappm05726.dev;S.browser_fallback_url=https://api.dev.src.id/link/dashboard;end">Buka di Aplikasi PMI</a>
+      <a href="intent://link/ovo#Intent;scheme=https;package=com.pmi.limited.pmiappm05726.dev;S.browser_fallback_url=https://api.dev.src.id/link/dashboard;end">Buka di Aplikasi PMI PROD</a>
+      <a href="intent://link/ovo#Intent;scheme=https;package=com.pmi.limited.pmiappm05726.dev;S.browser_fallback_url=https://api.dev.src.id/link/dashboard;end">Buka di Aplikasi PMI DEV</a>
+      <a href="intent://link/ovo#Intent;scheme=https;package=com.pmi.limited.pmiappm05726.dev;S.browser_fallback_url=https://api.dev.src.id/link/dashboard;end">Buka di Aplikasi PMI QA</a>
       <Hero />
       <About />
       <Menu />
